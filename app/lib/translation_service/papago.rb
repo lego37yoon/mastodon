01e1119fedcb37f78ab78e2540fc400a8340d775
paperbox <lego37yoon@outlook.com>
2023-08-01 15:21:01 +0900

    test papago api with another data type

--- app/lib/translation_service/papago.rb
+++ app/lib/translation_service/papago.rb
@@ -28,7 +28,7 @@ class TranslationService::Papago < TranslationService
   private
 
   def request(text, source_language, target_language)
-    req = Request.new(:post, 'https://openapi.naver.com/v1/papago/n2mt', form: "source=#{source_language}&target=#{target_language}&text=#{text}")
+    req = Request.new(:post, 'https://openapi.naver.com/v1/papago/n2mt', form: {source: source_language, target: target_language, text:text})
     req.add_headers('Content-Type': "application/x-www-form-urlencoded; charset=UTF-8")
     req.add_headers('X-Naver-Client-Id': "#{@api_id}")
     req.add_headers('X-Naver-Secret': "#{@api_secret}")
--- lib/mastodon/version.rb
+++ lib/mastodon/version.rb
@@ -21,7 +21,7 @@ module Mastodon
     end
 
     def suffix
-      '+smore+patch.3'
+      '+smore+patch.4'
     end
 
     def to_a
